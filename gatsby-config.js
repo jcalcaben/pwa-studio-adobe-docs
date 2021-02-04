53c2efba4b6b1978d5875efdb2a62e38fc09c3fb
James Calcaben <calcaben@adobe.com>
2021-02-04 15:58:00 -0600

    Update path prefix

--- gatsby-config.js
+++ gatsby-config.js
@@ -151,5 +151,5 @@ module.exports = {
     ]
   },
   plugins: [`@adobe/gatsby-theme-aio`],
-  pathPrefix: process.env.PATH_PREFIX || '/dev-site-documentation-template/'
+  pathPrefix: process.env.PATH_PREFIX || '/pwa-studio-adobe-docs/'
 };
